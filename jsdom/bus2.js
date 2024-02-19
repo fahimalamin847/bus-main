7d86749bac2a545cf6ebba5c5ac16a398d30b03d
fahimalamin847 <fi0928986@gmail.com>
2024-02-20 01:28:13 +0600

    first commit

---
+++ jsdom/bus2.js
@@ -0,0 +1,8 @@
+function nxtprt(){
+    const nextbutton = document.getElementById('next-btn')
+    nextbutton.classList.add('hidden')
+
+    //sucess
+    const sucessPrt =document.getElementById('sucess')
+    sucessPrt.classList.remove('hidden')
+}
---
+++ jsdom/bus3.js
@@ -0,0 +1,8 @@
+function fahimPrt(){
+    const nextbutton = document.getElementById('next')
+    nextbutton.classList.add('hidden')
+
+    //sucess
+    const sucessPrt =document.getElementById('suc')
+    sucessPrt.classList.remove('hidden')
+}
